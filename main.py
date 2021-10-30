c41d178d1a565f7e5b118f9d678c4c32655fe47b
menuflex <79350810+bensinssh@users.noreply.github.com>
2021-10-30 22:48:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(os.environ['ACCESS_TOKEN'])
-handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
+line_bot_api = LineBotApi(os.environ['2CNmJsIBAOJ1g/UbGV/NquzQitvnT+/RV+hkJmdJdt3S5pXfgF1lZrLnqNUL17fT3/scIGdWNmeH1P0mpIhFDTuy54MPJARb7FPKVOqy4x1KPLfS5jDwnOg7FPKVOqy4x1KPLfS5jDwnOg4x2'​])
+handler = WebhookHandler(os.environ['e34f51f308839a2ff250c2c49c6c9046'])
 
 
 @app.route('/')
